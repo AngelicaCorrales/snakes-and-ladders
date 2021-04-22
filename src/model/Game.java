bf1d8f06ba8447ac769b1ef526a1230691ed3627
Keren lopez <kerenlopezcordoba@outlook.es>
2021-04-22 16:02:27 -0500

    Changes in Main and Game, some methods were created

--- src/model/Game.java
+++ src/model/Game.java
@@ -1,9 +1,49 @@
 package model;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Game {
+	
+	private Grid grid;
 
 	public Game() {
 		
+	}
+	
+	public void startGame(int rows, int cols, int snakes, int ladders, String players) {
+		int playersN = -1;
+		String playersS = "";
+		try {
+			playersN = Integer.parseInt(players);
+		}catch(NumberFormatException num) {
+			playersS = players;
+		}
+		if(playersN!=-1) {
+			String playersA = assignSymbol(playersN);
+			grid = new Grid(playersA, ladders, rows, cols);
+		}
+		if(!playersS.equals("")) {
+			grid = new Grid(playersS, snakes, ladders, rows, cols);
+		}
+	}
+	
+	public String assignSymbol(int playersN) {
+		String symbol1 = "*";
+		String symbol2 = "!";
+		String symbol3 = "O";
+		String symbol4 = "X";
+		String symbol5 = "%";
+		String symbol6 = "$";
+		String symbol7 = "#";
+		String symbol8 = "+";
+		String symbol9 = "&";
+		String players = "";
+		int cont = 0;
+		int codeNum = 0;
+		if(cont!=playersN) {
+			codeNum = ThreadLocalRandom.current().nextInt(1, 10);
+		}
+		return players;
 	}
 	
 	public void moveGame() {
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,10 +1,116 @@
 package ui;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import model.Game;
+
 public class Main {
-
-	public static void main(String[] args) {
-		
-
+	
+	private static BufferedReader br;
+	private static BufferedWriter bw;
+	private Game mainGame;
+	
+	public Main(){
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		mainGame = new Game();
 	}
 
+	public static void main(String[] args) throws IOException {
+		bw.write("*******************************************************************************************************************************************************************");
+		bw.write("\n                                                             ¡Bienvenido a Snakes and Ladders!                                                                     ");
+		bw.write("\n*******************************************************************************************************************************************************************");
+		Main objMain = new Main();
+		int option = 0;
+		do{
+			option = objMain.menu();
+		} while(option!= 3);
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+	
+	public int menu() throws IOException{
+		int option = 0;
+		boolean menu = true;
+		boolean pass = true;
+		do {
+			try {
+				while(menu){
+					bw.write(
+						"\n**********************************************************************************\n"+
+						"                         Seleccione una opcion para continuar"+
+						"\n**********************************************************************************\n"+
+						"      1. Comenzar juego\n"+
+						"      2. Ver el tablero de posiciones\n"+
+						"      3. Finalizar el programa\n"
+					);
+					option = Integer.parseInt(br.readLine());
+					switch(option){
+						case 1:
+							startGame();
+							menu = false;
+							pass = false;
+							break;
+						case 2:
+							showPositionsBoard();
+							menu = false;
+							pass = false;
+							break;
+						case 3:
+							bw.write("\n**********************************************************************************\n"+"Gracias por jugar"+"\n**********************************************************************************\n");
+							menu = false;
+							pass = false;
+							break;
+						default:
+							bw.write("Ingrese una opcion valida");
+							menu = false;
+							pass = false;
+							break;	
+					}		
+				} 
+			}catch(NumberFormatException num) {
+				bw.write("Debe ingresar un numero entero que corresponda con las opciones presentadas.");
+			}
+		}while(pass);
+		return option;
+	}
+	
+	public void startGame() throws IOException {
+		bw.write("\nPor favor ingrese el tamaño del tablero seguido de el numero de serpientes, escaleras y jugadores. Puede incluir los simbolos de los jugadores si lo desea, de lo contrario seran asignados aleatoriamente.");
+		String line = br.readLine();
+		if(line!=null && !line.equals("")) {
+			String[] info = br.readLine().split(" ");
+			int rows = Integer.parseInt(info[0]);
+			int cols = Integer.parseInt(info[1]);
+			int snakes = Integer.parseInt(info[2]);
+			int ladders = Integer.parseInt(info[3]);
+			String players = info[4];
+			mainGame.startGame(rows, cols, snakes, ladders, players);
+		}
+		/*while(line!=null){
+			int availableBooks = Integer.parseInt(line);
+			String[] prices = br.readLine().split(" ");
+			int peterMoney = Integer.parseInt(br.readLine());
+			arrayOfPrices = new int[availableBooks];
+			for(int k = 0;k<prices.length;k++) {
+				arrayOfPrices[k] = Integer.parseInt(prices[k]);
+			}
+			Arrays.sort(arrayOfPrices);
+			String message = findBooks(peterMoney);
+			bw.write(message+"\n\n");
+			line = br.readLine();
+			line = br.readLine();
+		}*/
+	}
+	
+	public void showPositionsBoard() {
+		
+	}
 }
